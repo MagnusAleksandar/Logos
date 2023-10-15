6ace87df4425b96658fa2d8477bce7d4dc8f1c09
MagnusAleksandar <alevel.col@gmail.com>
2023-10-14 20:26:57 -0500

    'γ' added

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -6,7 +6,7 @@ public class JavaApplication10 {
     private static Logos l = new Logos();
 
     public static void main(String[] args) {
-        String op = "[(a*b)+(c*d)]*[(e*f)+(-g)]";
+        String op = "[(a*b)+(c*d)]*[(e*f)+(γ)]";
         l.gather(op);
         // l.finalOp();
     }
