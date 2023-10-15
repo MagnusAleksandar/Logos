67cc5d2cc2bec47ec44ff752d853bae8d109f0fe
MagnusAleksandar <alevel.col@gmail.com>
2023-10-14 20:24:11 -0500

    Minus added for single variable

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -6,7 +6,7 @@ public class JavaApplication10 {
     private static Logos l = new Logos();
 
     public static void main(String[] args) {
-        String op = "[(a*b)+(c*d)]*[(e*f)+(g)]";
+        String op = "[(a*b)+(c*d)]*[(e*f)+(-g)]";
         l.gather(op);
         // l.finalOp();
     }
